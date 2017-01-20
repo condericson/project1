f6de406e52261ec11faf67fabfe09337591a8ef5
Connor's laptop <condericson@gmail.com>
2017-01-19 21:01:30 -0500

    first init

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+	console.log('app is running');
+})
